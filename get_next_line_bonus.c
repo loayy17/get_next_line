40be81e0880cecf02d3327518038ea8a688e6c1c
loayy17 <101842298+loayy17@users.noreply.github.com>
2024-12-25 23:10:35 +0300

    done

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,16 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lalhindi <lalhindi@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 21:53:26 by lalhindi          #+#    #+#             */
-/*   Updated: 2024/12/25 23:09:26 by lalhindi         ###   ########.fr       */
+/*   Updated: 2024/12/25 23:03:22 by lalhindi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
+#define MAX_FD 1024
 
 int	ft_free(char *buffer, char *save)
 {
@@ -18,6 +19,7 @@ int	ft_free(char *buffer, char *save)
 	free(save);
 	return (1);
 }
+
 char	*ft_line(char *remain_str)
 {
 	int		i;
@@ -59,13 +61,13 @@ char	*ft_save(char *remain_str)
 	if (!remain_str[i])
 	{
 		free(remain_str);
-		return (NULL);
+		return (0);
 	}
 	save = malloc(ft_strlen(remain_str) - i + 1);
 	if (!save)
 	{
 		free(save);
-		return (NULL);
+		return (0);
 	}
 	i++;
 	j = 0;
@@ -104,18 +106,18 @@ char	*ft_read_save(int fd, char *save)
 
 char	*get_next_line(int fd)
 {
-	static char *remain_str;
-	char *line;
+	static char	*remain_str[MAX_FD];
+	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd >= MAX_FD)
 	{
 		return (0);
 	}
-	remain_str = ft_read_save(fd, remain_str);
-	if (!remain_str)
+	remain_str[fd] = ft_read_save(fd, remain_str[fd]);
+	if (!remain_str[fd])
 		return (0);
-	line = ft_line(remain_str);
-	remain_str = ft_save(remain_str);
+	line = ft_line(remain_str[fd]);
+	remain_str[fd] = ft_save(remain_str[fd]);
 	if (!line[0])
 	{
 		free(line);
---
+++ get_next_line_bonus.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+#define GET_NEXT_LINE_BONUS_H
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 100
+#endif
+
+int ft_strlen(char *s);
+char *ft_strdup(char *s);
+char *ft_strchr(const char *s, int c);
+char *ft_strjoin(char *s1, char *s2);
+char *ft_save(char *remain_str);
+char *ft_line(char *remain_str);
+char *get_next_line(int fd);
+
+#endif
